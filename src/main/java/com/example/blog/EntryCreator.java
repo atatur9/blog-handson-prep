f3648ab03b4380041bf4a04f5d694facc56dd640
atatur9 <momodarou@gmail.com>
2018-01-27 21:31:20 +0900

    prep

--- src/main/java/com/example/blog/EntryCreator.java
+++ src/main/java/com/example/blog/EntryCreator.java
@@ -1,5 +1,6 @@
 package com.example.blog;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 import am.ik.blog.entry.*;
@@ -11,13 +12,24 @@ public class EntryCreator {
 	public static Entry createEntry(EntryId entryId, Content content, Title title,
 			Categories categories, Tags tags, Author created, Author updated) {
 		// TODO: 実装してください
-		return Entry.builder().build();
+		return Entry.builder()
+				.entryId(entryId)
+				.content(content)
+				.created(created)
+				.frontMatter(new FrontMatter(title, categories, tags))
+				.updated(updated)
+				.build()
+				.useFrontMatterDate();
 	}
 
 	public static Optional<Entry> createEntryFromMarkdown(Resource resource,
 			Author created, Author updated) {
 		EntryFactory factory = new EntryFactory();
-		// TODO: 実装してください
-		return Optional.empty();
+		return factory.createFromYamlFile(resource)
+				.map(builder -> builder
+						.entryId(EntryId.fromFileName(resource.getFilename()))
+						.created(created)
+						.updated(updated)
+						.build());
 	}
 }
--- src/main/java/com/example/blog/webhook/EntryFetcher.java
+++ src/main/java/com/example/blog/webhook/EntryFetcher.java
@@ -2,13 +2,16 @@ package com.example.blog.webhook;
 
 import java.nio.file.Paths;
 
-import am.ik.blog.entry.Entry;
-import am.ik.blog.entry.EntryId;
+import am.ik.blog.entry.*;
+import am.ik.blog.entry.factory.EntryFactory;
 import am.ik.github.GitHubClient;
+import am.ik.github.core.Committer;
 import am.ik.github.repositories.commits.CommitsResponse.Commit;
 import am.ik.github.repositories.contents.ContentsResponse.File;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 public class EntryFetcher {
 	private final GitHubClient gitHubClient;
@@ -21,9 +24,32 @@ public class EntryFetcher {
 		EntryId entryId = EntryId.fromFilePath(Paths.get(path));
 		Mono<File> file = this.gitHubClient.file(owner, repo, path).get();
 		Flux<Commit> commits = this.gitHubClient.commits(owner, repo)
-				.get(params -> params.path(path));
+		 		.get(params -> params.path(path));
+		Mono<Tuple2<Author, Author>> authors = commits.collectList()
+				.map(list -> {
+					Author updated = toAuthor(list.get(0));
+					Author created = toAuthor(list.get(list.size() - 1));
+					return Tuples.of(created, updated);
+				});
 
-		// TODO: 実装してください
-		return Mono.empty();
+		Mono<Entry.EntryBuilder> builder = file.map(f -> new EntryFactory().parseBody(entryId, f.decode()))
+				.flatMap(b -> Mono.justOrEmpty(b));
+
+		return Mono.zip(builder, authors)
+				.map(t -> {
+					Entry.EntryBuilder entryBuilder = t.getT1();
+					Author created = t.getT2().getT1();
+					Author updated = t.getT2().getT2();
+					return entryBuilder
+							.created(created)
+							.updated(updated)
+							.build()
+							.useFrontMatterDate();
+				});
+	}
+
+	private static Author toAuthor(Commit commit) {
+		Committer committer = commit.getCommit().getAuthor();
+		return new Author(new Name(committer.getName()), new EventTime(committer.getDate()));
 	}
 }
